89d67d2000450fd3efcee6f8ff8371abfffa71f5
Manish kumar <98228493+Manishganak1919@users.noreply.github.com>
2023-07-15 06:38:09 +0530

    first code

---
+++ server/connectMongoDb.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async() => {
+  try {
+    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected successfully`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
